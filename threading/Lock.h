a9fa813b6d20de7bca60e37bb90d013010cf4e87
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-04-13 13:41:27 -0600

    Mutexes are now working
    The output of the example program will sometimes be mixed between
    thread1 and thread2 because the cout object is not protected by a mutex
    or similar.

--- threading/Lock.h
+++ threading/Lock.h
@@ -20,6 +20,44 @@ public:
     void unlock();
 };
 
+
+#include <stdio.h>
+
+inline
+Lock::Lock()
+{
+    int ret;
+    if (ret = pthread_mutex_init(&mMutex, NULL) != 0)
+    {
+        perror("pthread_mutex_init ");
+    }
+}
+
+inline
+Lock::~Lock()
+{
+    int ret;
+    if (ret = pthread_mutex_destroy(&mMutex) != 0)
+    {
+        perror("pthread_mutex_destroy ");
+    }
+}
+
+
+inline
+void Lock::lock()
+{
+    pthread_mutex_lock(&mMutex);
+}
+
+inline
+void Lock::unlock()
+{
+    pthread_mutex_unlock(&mMutex);
+}
+
+///////////////////////////////=============================/////////////////////////
+
 class ScopedLock
 {
 private:
@@ -29,6 +67,19 @@ public:
     ScopedLock(Lock &lock);
     virtual ~ScopedLock();
 };
+
+inline
+ScopedLock::ScopedLock(Lock &lock)
+    :mLock(lock)
+{
+    mLock.lock();
+}
+
+inline
+ScopedLock::~ScopedLock()
+{
+    mLock.unlock();
+}
 
 }
 
--- threading/main.cpp
+++ threading/main.cpp
@@ -1,9 +1,58 @@
 
 #include "periodicrtthread.h"
+#include "Lock.h"
+using namespace USU;
 
 #include <iostream>
 using namespace std;
-using namespace USU;
+
+class SharedObject
+{
+private:
+    Lock mLock1;
+    Lock mLock2;
+    int mData1;
+    int mData2;
+
+public:
+    SharedObject():mData1(0), mData2(0)
+    {}
+    void increaseData1()
+    {
+        mLock1.lock(); // Lock mutex manually
+            mData1++;
+        mLock1.unlock(); // Unlock mutex manually
+    }
+
+    void increaseData2()
+    {
+        /*
+         * ScopedLock locks the mutex when its created
+         * and unlocks it automatically when it is destroyed
+         * at the end of its scope. Therefore the mutex will be
+         * unlocked even if the code returns in an
+         * unexpected way (exception or error)
+         */
+        ScopedLock scLock(mLock2);
+        mData2++;
+    }
+
+    int getData1()
+    {
+        int temp; // needed to unlock mutex before return
+        mLock1.lock();
+            temp = mData1;
+        mLock1.unlock();
+        return temp;
+    }
+
+    int getData2()
+    {
+        ScopedLock scLock(mLock2);
+        return mData2;
+    } // Mutex will be released automatically here
+};
+
 
 class MyThread: public PeriodicRtThread
 {
@@ -14,33 +63,32 @@ public:
 
     void run()
     {
-        cout << "Hello from Thread" << endl;
+        SharedObject* counter = reinterpret_cast<SharedObject*> (mArgs);
+        cout << "Hello from Thread: " << mId << endl;
         for(int i = 0; i<10; i++)
         {
-            struct sched_param param;
-            int policy;
-            pthread_getschedparam(mId, &policy, &param);
+            counter->increaseData1();
+            counter->increaseData2();
 
-            cout << "policy=" <<  ((policy == SCHED_FIFO)  ? "SCHED_FIFO" :
-                                  (policy == SCHED_RR)    ? "SCHED_RR" :
-                                  (policy == SCHED_OTHER) ? "SCHED_OTHER" :
-                                                                                                                "???")
-                 << ", priority=" << param.sched_priority << endl;
+            cout << "Thread " << mId << ": Data1 " << counter->getData1() << endl;
+            cout << "Thread " << mId << ": Data2 " << counter->getData2() << endl;
             waitPeriod();
         }
     }
 };
 
-
 int main()
 {
-    cout << "Creating thread" << endl;
-    MyThread thr(5);
-    cout << "Starting thread" << endl;
-    thr.start();
+    cout << "Creating threads" << endl;
+    MyThread thr1(5), thr2(4);
+    SharedObject sO;
+    cout << "Starting threads" << endl;
+    thr1.start((void*) &sO);
+    thr2.start((void*) &sO);
 
-    thr.join();
-    cout << "Thread joined" << endl;
+    thr1.join();
+    thr2.join();
+    cout << "Threads joined" << endl;
 
 
  return 0;
