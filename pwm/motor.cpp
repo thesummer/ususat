dc75c9195caa8821cb20dd82dc36c3222f3552b4
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-04-22 11:13:15 -0600

    Add authorship of motor-class

--- pwm/motor.cpp
+++ pwm/motor.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file motor.cpp
+ *
+ * Class for motor interface consisting of pwm input signal
+ * and 2 digital pins for enable and direction.
+ *
+ * @author Jan Sommer
+ *  Created on: Apr 22, 2013
+ *
+ */
+
 #include "motor.h"
 using namespace USU;
 
--- pwm/motor.h
+++ pwm/motor.h
@@ -1,5 +1,17 @@
+/**
+ * @file motor.h
+ *
+ * Class for motor interface consisting of pwm input signal
+ * and 2 digital pins for enable and direction.
+ *
+ * @author Jan Sommer
+ *  Created on: Apr 22, 2013
+ *
+ */
+
 #ifndef MOTOR_H
 #define MOTOR_H
+
 
 namespace USU
 {
