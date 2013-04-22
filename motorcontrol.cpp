3a552255691ee29a4271cf8e14f58b90b8e81e80
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-04-22 16:44:33 -0600

    Add doxygen comments to motorcontrol class

--- motorcontrol.cpp
+++ motorcontrol.cpp
@@ -16,6 +16,8 @@ using namespace USU;
 MotorControl::MotorControl(int priority, unsigned int period_us, KalmanFilter &kalmanfilter)
     :PeriodicRtThread(priority, period_us), mPwm1(0), mPwm2(1), mKalmanFilter(kalmanfilter)
 {
+    // Initizalize the four motors
+    ///TODO: use meaningful Pin numbers (declare consts)
     mMotor[0] = new Motor(mBeagleGpio, 1,2, &mPwm1.DutyA_percent);
     mMotor[1] = new Motor(mBeagleGpio, 1,2, &mPwm1.DutyB_percent);
     mMotor[2] = new Motor(mBeagleGpio, 1,2, &mPwm2.DutyA_percent);
@@ -25,6 +27,7 @@ MotorControl::MotorControl(int priority, unsigned int period_us, KalmanFilter &k
 void MotorControl::run()
 {
     mKeepRunning = true;
+
     while(mKeepRunning)
     {
         bool temp = mKalmanFilter.getState();
--- motorcontrol.h
+++ motorcontrol.h
@@ -23,27 +23,59 @@
 namespace USU
 {
 
+/*!
+ \brief Represents the Periodic task for motor control
+
+ This class is derived from PeriodicRtThread. It initializes the interface to the
+ 4 motors. In a periodic loop it takes the last system state estimate from the Kalman
+ filter, calculates the appropiate control response and sets the speed (duty cycle)
+ of the motors.
+*/
 class MotorControl: public PeriodicRtThread
 {
 public:
 
 
+    /*!
+     \brief Constructor of the class
+
+     Initializes the underlying PeriodicRtThread, the GPIO-class,
+     the PWMs and the 4 Motors.
+
+     \param priority  priority of the periodic pthread
+     \param period_us   period (in us) of the periodic pthread
+     \param kalmanfilter reference to the KalmanFilter to get state estimates
+    */
     MotorControl(int priority = 0 , unsigned int period_us = 1000000,
                  KalmanFilter& kalmanfilter);
 
+    /*!
+     \brief Signals the thread to stop
+    */
     void stop() { mKeepRunning = false; }
+
+    /*!
+     \brief Thread routing
+
+     - Gets the newest estimate from KalmanFilter
+     - Calculate the control response
+     - Set the motor speed of the 4 Motors
+     - wait for the next timer event
+     TODO: Its only an idea, no actual implementation yet.
+    */
     virtual void run();
 
 private:
-    cPWM::cPWM mPwm1;
-    cPWM::cPWM mPwm2;
+    cPWM::cPWM mPwm1; /*!< First PWM module (has 2 channels) */
+    cPWM::cPWM mPwm2; /*!< Second PwM module (has 2 channels) */
 
-    Beagle_GPIO mBeagleGpio;
-    Motor mMotor[4];
-    KalmanFilter& mKalmanFilter;
-    volatile bool mKeepRunning;
+    Beagle_GPIO mBeagleGpio; /*!< Representation of the BeagleBoard Gpios (access via mmap) */
+    Motor mMotor[4]; /*!< Array of the 4 motors */
+    KalmanFilter& mKalmanFilter; /*!< Reference to the KalmanFilter */
+    volatile bool mKeepRunning; /*!< Indicates if the Thread should keep running. volatile to prevent optimizing */
 
     MotorControl(const MotorControl& thread); /*!< Copy constructor made inaccessible by declaring it private */
+
     MotorControl& operator=(const MotorControl& rhs); /*!< Assignment constructor made inaccessible by declaring it private */
 };
 
