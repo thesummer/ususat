4f24e2d5772c30e6ff87e53a43911b8f6f66ae15
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-04-24 19:26:17 -0600

    Implemented some Commands

--- 3dm/messages.h
+++ 3dm/messages.h
@@ -1,6 +1,8 @@
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
+// Command protocol constants
+
 const unsigned int RAW_ACC_ANG                     = 0xC1; /*!< Raw Accelerometer and Angular Rate Sensor Outputs */
 const unsigned int ACC_ANG                         = 0xC2; /*!< Acceleration & Angular Rate */
 const unsigned int DELTA_ANGLE_VEL                 = 0xC3; /*!< DeltaAngle & DeltaVelocity */
@@ -36,5 +38,166 @@ const unsigned int READ_DEVICE_ID                  = 0xEA; /*!< Read Device ID S
 const unsigned int STOP_CONTINUOUS                 = 0xFA; /*!< Stop Continuous Mode (no reply) */
 const unsigned int FIRMWARE_UPDATE                 = 0xFD; /*!< Firmware Update (no reply) */
 const unsigned int DEVICE_RESET                    = 0xFE; /*!< Device Reset (no reply) */
+
+#include "../minimu/vector.h"
+#include<stdint.h>
+
+class GX3Packet
+{
+public:
+    virtual unsigned int size() = 0;
+    static bool calculateChecksum(uint8_t * buffer, unsigned int length)
+    {
+        uint16_t sum = 0;
+        for(unsigned int i = 0; i<length-2; i++)
+        {
+            sum += buffer[i];
+        }
+        return (sum == (*(uint16_t) &buffer[length-2]) );
+    }
+
+    static inline vector createVector(uint8_t * buffer)
+    {
+        return vector(*(float*) &buffer[0],
+                      *(float*) &buffer[4],
+                      *(float*) &buffer[8]);
+    }
+
+    static void createMatrix(uint8_t * buffer, matrix& mat)
+    {
+        mat << *(float*) &buffer[0],  *(float*) &buffer[4],  *(float*) &buffer[8],
+               *(float*) &buffer[12], *(float*) &buffer[16], *(float*) &buffer[20],
+               *(float*) &buffer[24], *(float*) &buffer[28], *(float*) &buffer[32];
+    }
+};
+
+class RawAccAng : public GX3Packet
+{
+public:
+    RawAccAng(uint8_t* buffer)
+    {
+        acc  = createVector(&buffer[1]);
+        gyro = createVector(&buffer[13]);
+
+        timer = *(unsigned int*) &buffer[25];
+    }
+
+    virtual unsigned int size() {return mSize;}
+
+    vector acc;
+    vector gyro;
+
+    unsigned int timer;
+
+private:
+    const static int mSize = 31;
+};
+
+
+class AccAngMagOrientationMat : public GX3Packet
+{
+public:
+    AccAngMagOrientationMat(uint8_t* buffer)
+    {
+        acc  = createVector(&buffer[1]);
+        gyro = createVector(&buffer[13]);
+        mag  = createVector(&buffer[25]);
+
+        createMatrix(&buffer[37], orientation);
+        timer = *(unsigned int*) &buffer[73];
+    }
+
+    virtual unsigned int size() {return mSize;}
+
+    vector acc;
+    vector gyro;
+    vector mag;
+
+    matrix orientation;
+    unsigned int timer;
+
+private:
+    const static int mSize = 79;
+};
+
+class GX3Command
+{
+public:
+    virtual unsigned int size() = 0;
+
+};
+
+class SamplingSettings : public GX3Command
+{
+public:
+    enum FunctionSelector{
+        ReturnOnly=0, Change=1,
+        ChangeAndSave=2, ChangeWithoutReply=3
+    };
+    enum DataConditioning{
+        CalcOrientation      = 0x01,
+        EnableConingSculling = 0x02,
+        FloatLittleEndian    = 0x10,
+        SuppressNaN          = 0x20,
+        FiniteSizeCorrection = 0x40,
+        DisableMag           = 0x100,
+        DisableMagNorthComp  = 0x400,
+        DisableGravComp      = 0x800,
+        EnableQuaternion     = 0x1000
+
+    };
+
+    SamplingSettings(FunctionSelector funSel, uint16_t samplingPeriod_ms = 10,
+                     uint16_t dataCondFlags = 0x03, uint8_t gyroAccFilter = 15,
+                     uint8_t magFilter = 17, uint16_t upCompensation = 10,
+                     uint16_t northCompensation = 10, uint8_t magPower = 0)
+    {
+        uint16_t *temp16;
+        mCommand[0] = SAMPLING_SETTINGS;
+        mCommand[1] = 0xA8;
+        mCommand[2] = 0xB9;
+        mCommand[3] = (uint8_t) funSel;
+        temp16 = (uint16_t*) &mCommand[4];
+        *temp16 = samplingPeriod_ms;
+        temp16 = (uint16_t*) &mCommand[6];
+        *temp16 = dataCondFlags;
+        mCommand[8]  = gyroAccFilter;
+        mCommand[9]  = magFilter;
+        temp16 = (uint16_t*) &mCommand[10];
+        *temp16 = upCompensation;
+        temp16 = (uint16_t*) &mCommand[12];
+        *temp16 = northCompensation;
+        mCommand[14] = magPower;
+        mCommand[15] = 0;
+        mCommand[16] = 0;
+        mCommand[17] = 0;
+        mCommand[18] = 0;
+        mCommand[19] = 0;
+
+    }
+
+    virtual unsigned int size() {return mSize;}
+    bool checkResponse(uint8_t *buffer, unsigned int length)
+    {
+        if (length != 19) return false;
+
+        if(GX3Packet::calculateChecksum(buffer, length) == false)
+            return false;
+
+        if(buffer[0] != SAMPLING_SETTINGS) return false;
+        for(int i=1; i<11; i++)
+        {
+            if(buffer[i] != mCommand[i+3]) return false;
+        }
+
+        return true;
+    }
+
+    uint8_t mCommand[mSize];
+
+private:
+    const static int mSize = 20;
+};
+
 
 #endif // MESSAGES_H
