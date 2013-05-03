061c7d26290cf2d68d08f59811f06f59a146a38d
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-05-02 18:42:04 -0600

    Added doxygen comments to sharedqueue.h

--- threading/sharedqueue.h
+++ threading/sharedqueue.h
@@ -21,23 +21,57 @@ using std::queue;
 namespace USU
 {
 template <class T>
+/*!
+ \brief Wrapper class to make std::queue thread safe
+
+ Protects the push, pop and front access from thread using
+ a mutex.
+ It can only handle one reader and one writer thread at a time.
+ Multiple reader threads could produce race conditions!!!
+
+*/
 class SharedQueue
 {
 public:
+    /*!
+     \brief Constructor, creates an empty queue
+
+    */
     SharedQueue();
 
+    /*!
+     \brief Adds a new element to the back of the queue
+
+     Takes the mutex before the write operation.
+
+     \param newElement the element to be added
+    */
     void push(const T& newElement)
     {
         ScopedLock(mLock);
         mQueue.push(newElement);
     }
 
+    /*!
+     \brief Destroys the first (oldest) element in the queue
+
+     Takes mutex before the write operation.
+     Calls the destroy operator of the current front-element.
+
+    */
     void pop()
     {
         ScopedLock (mLock);
         mQueue.pop();
     }
 
+    /*!
+     \brief Returns a reference to the first (oldest) element in the queue.
+
+     Takes a mutex before accesing the first element.
+
+     \return T
+    */
     T& front()
     {
         ScopedLock(mLock);
@@ -45,8 +79,8 @@ public:
     }
 
 private:
-    Lock mLock;
-    queue<T> mQueue;
+    Lock mLock; /*!< Mutex to protect the queue */
+    queue<T> mQueue; /*!< Standard queue to store the data */
 };
 
 }
