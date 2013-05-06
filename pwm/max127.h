bd5080d974dbf24cd3ba9db41677f456c5e6e891
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-05-06 16:12:16 -0600

    Add doxygen comment in max127.h

--- pwm/max127.h
+++ pwm/max127.h
@@ -6,20 +6,69 @@
 #include "../minimu/I2CBus.h"
 
 
-const uint8_t I2C_ADDRESS  = 0b00101000;
-const uint8_t CONTROL_BYTE = 0b10000110;
-const uint8_t SEL0         = 4;
+/*!
+ \brief I2C-address of the ADC
 
+ It is assumed that the PINs A0-A2 are connected to GND.
+ If the PINs are connected by VCC change accordingly.
+*/
+const uint8_t I2C_ADDRESS  = 0b00101000;
+
+/*!
+ \brief Template of the control byte
+
+ The used settings are_
+    - fullscale range +-5V
+    - Standby Power-Down mode
+
+ The bits for channel selection are set to 0.
+ Send CONTROL_BYTE | (CH<<SEL0) with CH being the
+ desired channel via the I2CBus.
+*/
+const uint8_t CONTROL_BYTE = 0b10000110;
+const uint8_t SEL0         = 4; /*!< Bit offset for channel selection */
+
+/*!
+ \brief Class representing the MAX127 ADC
+
+ Provides simple functionality to read the channels.
+ Uses the I2CBus class for communication.
+
+*/
 class Max127
 {
 public:
+   /*!
+    \brief Constructor
+
+    Initializes the I2C-connection
+
+    \param i2cdevice device name of the i2c-bus (e.g. /dev/i2c-1)
+   */
     Max127(char *i2cdevice);
 
+    /*!
+     \brief Returns the raw integer measurement of the selected channel
+
+     At the moment assumens bipolar operation.
+     The range is [-2048, 2047]
+
+     \param channel channel to read
+     \return int16_t signed integer representing the measurement
+    */
     int16_t readRaw(uin8_t channel);
+    /*!
+     \brief Returns the measurement of the selected channel in volts
+
+     At the moment assumes fullscale of 10 V (bipolar +-5V or unipolar)
+
+     \param channel channel to read
+     \return float  measured voltage in V
+    */
     float readVoltage(unsigned int channel);
 
 private:
-    I2CBus mI2c;
+    I2CBus mI2c; /*!< TODO */
 
 
 };
