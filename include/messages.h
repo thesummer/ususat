cd517cf9d8f47e004b9391d370a0fd66fa1e9c90
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-08-05 14:30:33 -0600

    Remove old debug lines

--- include/messages.h
+++ include/messages.h
@@ -204,12 +204,6 @@ public:
         if(GX3Packet::calculateChecksum(buffer, size) == false)
         {
             using namespace std;
-//            cout << "Checksum failed" << endl;
-//            for (int i = 0; i<size; ++i)
-//            {
-//                cout << hex <<  setw(2) << setfill('0') << "0x" << (int) buffer[i] << dec << "+ ";
-//            }
-//            cout << endl;
             return false;
         }
 
@@ -267,20 +261,13 @@ public:
         buffer[0] = serialPort.ReadByte();
         if(buffer[0] != ACC_ANG_MAG_VEC)
         {
-//            std::cout << (char) buffer[0] << std::endl;
             return false; //throw std::runtime_error("Wrong package identifier");
         }
 
         serialPort.ReadRaw(&buffer[1], size-1);
         if(GX3Packet::calculateChecksum(buffer, size) == false)
         {
-//            cout << "Checksum failed" << endl;
             using namespace std;
-//            for (int i = 0; i<size; ++i)
-//            {
-//                cout << hex <<  setw(2) << setfill('0') << "0x" << (int) buffer[i] << dec << "+ ";
-//            }
-//            cout << endl;
             return false;
         }
 
