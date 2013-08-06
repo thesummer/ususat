2894a022b5bb6cecc27879937ad0fa146738c624
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2013-08-06 15:13:24 -0600

    Add file with definitions of doxygen modules

---
+++ include/doxygen.h
@@ -0,0 +1,28 @@
+/**
+    \defgroup threading Utitlity classes for threading with pthread
+
+    Yadsjflsfjlk yadadada dadadljfsfj
+
+**/
+
+
+/**
+  \defgroup minimu Classes related to communication with Pololu MinIMU
+
+  TODO: Write something here
+**/
+
+
+/**
+  \defgroup 3dm Classes related to communication with MicroStrain 3DM-GX3
+
+  TODO: Write something here
+
+**/
+
+
+/**
+  \defgroup motor Classes related to controlling the motors
+
+  TODO: Write something here
+**/
